374cecf8a7474b63223ab3a9dee5daa5fc7a832f
viniputi <130999566+viniputi@users.noreply.github.com>
2025-01-17 18:57:31 +0700

    splash

--- app/_layout.js
+++ app/_layout.js
@@ -1,15 +1,30 @@
 // App.js
-import React from "react";
+import React,{ useEffect }  from "react";
 import { NavigationContainer, NavigationIndependentTree } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import LoginScreen from "./screens/LoginScreen";
 import RegisterScreen from "./screens/RegisterScreen";
 import HomeScreen from "./screens/HomeScreen";
 import EditProfileScreen from './screens/EditProfileScreen';
+import * as SplashScreen from 'expo-splash-screen';
 
 const Stack = createStackNavigator();
 
 export default function App() {
+  useEffect(() => {
+    // Menyembunyikan splash screen setelah 2 detik
+    async function prepare() {
+      console.log("Mencegah Splash Screen untuk disembunyikan");
+      await SplashScreen.preventAutoHideAsync(); // Menunggu splash screen tetap muncul
+      setTimeout(async () => {
+        console.log("Splash Screen disembunyikan");
+        await SplashScreen.hideAsync(); // Menyembunyikan splash screen setelah 5 detik
+      }, 5000);
+    }
+  
+    prepare();
+    
+  }, []);
   return (
     <NavigationIndependentTree>
         <NavigationContainer>
